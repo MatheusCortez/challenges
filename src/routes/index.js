07b380f69ba2e288012b613209cd063c61e26d87
Matheus Cortez <matheus.cortez@live.com>
2022-02-09 21:20:08 -0300

    feat:challenge finished

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 const {Router} =  require('express')
+const book = require('../controller/index')
 
 const router = Router()
 
+router.get('/',(req,res)=>{
+    res.send('Para Acessar a pagina do conteudo, vá para rota "/books"')
+})
 
-router.get('',(req,res)=>{
-    res.json({message:"Funcionou"})
+
+router.get('/books',(req,res)=>{
+    res.json(book)
 })
 
 module.exports=router;
